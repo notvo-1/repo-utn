5c16e9652717935d65eaa4c6cd2dd0b71a91663a
Notvo <friedrice1918@gmail.com>
2025-03-25 18:11:07 -0300

    no se que cambio

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,5 +1,5 @@
-nombre=input("Ingrese su nombre")
-apellido=input("Ingrese su apellido")
+nombre=input("Ingrese su nombre ")
+apellido=input("Ingrese su apellido ")
 materia="Programacion I"
 
 print(f"Hola soy {nombre} {apellido} y soy estudiante de {materia}")
